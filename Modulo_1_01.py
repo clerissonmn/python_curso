2041695010c07e4d818e8f859e4101ed656b4e89
clerissonmn <clerisson.mn@gmail.com>
2020-03-12 12:48:01 -0300

    commit inicial

---
+++ Modulo_1_01.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Curso de python
+
+Módulo 1
+
+"""
+
+
+#%%#######################
+### Tipos de variáveis ###
+##########################
+
+# Inteiro Int (inteiro)
+print(type(10))
+
+# Float (real)
+print(type(10.))
+
+# String (texto)
+print(type('10'))
+print(type("10"))
+
+# Booleanas
+print(type(True))
+print(type(False))
+
+
+
